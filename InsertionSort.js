d99a64c72ec03a1bc52ad895d93e85a8f8adc432
orlandouchiha425 <valadez425@gmail.com>
2023-02-05 12:33:10 -0500

    pushing to github

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,7 +1,13 @@
-function insertionSort(arr) {
-    newArr =[]
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
-        
+function insertionSort(arr){
+	var currentVal;
+    for(var i = 1; i < arr.length; i++){
+        currentVal = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentVal;
     }
+    return arr;
 }
+
+insertionSort([2,1,9,76,4])
