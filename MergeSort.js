4898f98e1fa561b3b26c8c8109fa22544ac101b3
orlandouchiha425 <valadez425@gmail.com>
2023-02-10 00:06:16 -0500

    mergesort push

--- MergeSort.js
+++ MergeSort.js
@@ -22,6 +22,17 @@ function merge(arr1, arr2){
     }
     return results;
 }
-merge([100,200], [1,2,3,5,6])
 
                    
+
+
+// Recrusive Merge Sort
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length/2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, sright);
+}
+
+mergeSort([10,24,76,73])
